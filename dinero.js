95b03c728a6f229f560f58cf6b6ac7f49bcede90
AveEMM <avery.mendez.escolar21@gmail.com>
2026-04-09 20:00:20 -0600

    Update currency conversion rates in dinero.js

--- dinero.js
+++ dinero.js
@@ -10,32 +10,32 @@ function escribir(n) {
 }
 
 function mxnUsd() {
-    mxUsa = Number(a) * 18.03;
+    mxUsa = Number(a) * 17.36;
     mostrarPantalla("MXN= " + Number(a) + "\n" + "USD= " + mxUsa); 
 }
 
 function mxnEur() {
-    mxUe = Number(a) / 20.85;
+    mxUe = Number(a) / 20.30;
     mostrarPantalla("MXN= " + Number(a) + "\n" + "EUR= " + mxUe);
 }
 
 function usdMxn() {
-    usaMx = Number(a) / 18.03;
+    usaMx = Number(a) / 17.36;
     mostrarPantalla("USD= " + Number(a) + "\n" + "MXN= " + usaMx);
 }
 
 function usdEur() {
-    usaUe = Number(a) * 0.92;
+    usaUe = Number(a) * 0.86;
     mostrarPantalla("USD= " + Number(a) + "\n" + "EUR= " + usaUe);
 }
 
 function eurMxn() {
-    ueMx = Number(a) * 20.85;
+    ueMx = Number(a) * 20.31;
     mostrarPantalla("EUR= " + Number(a) + "\n" + "MXN= " + ueMx);
 }
 
 function eurUsd() {
-    ueUsd = Number(a) * 1.08;
+    ueUsd = Number(a) * 1.16;
     mostrarPantalla("EUR= " + Number(a) + "\n" + "USD= " + ueUsd);
 }
 
